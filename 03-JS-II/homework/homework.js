ca0937828e2ab94dbfe9c9251341f940934b4461
andinosan <andinosan@gmail.com>
2022-06-03 23:32:59 -0300

    hasta tabladelseis

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -247,9 +247,20 @@ function tablaDelSeis(){
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
   let arrayTablaDel6 = [];
-  for (let i = 0; i < 11; i++) {
-        arrayTablaDel6.push(6 * i);
+  var numtabla = 0;
+  var resmul;
+
+  // for (let i = 0; i < 11; i++) {
+  //   arrayTablaDel6.push(6 * i);
+  // }
+
+  while (resmul !== 60)
+  {
+    resmul = numtabla * 6;
+    numtabla = numtabla + 1;
+    arrayTablaDel6.push(resmul);
   }
+
   return arrayTablaDel6;
 }
 
