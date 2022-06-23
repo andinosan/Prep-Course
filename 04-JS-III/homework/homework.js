780a493375b43f3c316a87a6ac1586407d12c987
andinosan <andinosan@gmail.com>
2022-06-23 15:52:13 -0300

    arraycontiene

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -59,11 +59,7 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
-  var newpalabras;
-  for (var i = 0; i < palabras.length; i++) {
-    newpalabras = `${newpalabras} ${palabras[i]}`;
-  }
-  return newpalabras;
+  return palabras.join(' ');
 }
 
 
@@ -71,6 +67,10 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  for (var i = 0; i < array.length; i++) {
+    if(array[i] === elemento) return true;
+  }
+  return false;
 }
 
 
