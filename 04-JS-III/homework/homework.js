b4b7a850f272aff82493f4b133b1b55ea64c7e64
andinosan <andinosan@gmail.com>
2022-06-23 16:40:30 -0300

    mesesdelaño

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -163,7 +163,6 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  var todosiguales;
   for (var i = 1; i < arreglo.length; i++) {
     if(arreglo[i]!==arreglo[i-1]) return false;
   }
@@ -176,6 +175,12 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var newarray = [];
+  for (var i = 0; i < array.length; i++) {
+    if(array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre")
+    newarray.push(array[i]);
+  }
+  return newarray;
 }
 
 
