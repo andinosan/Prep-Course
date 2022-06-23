d0a2150b0e5385618e0a16324ece5207305aea69
andinosan <andinosan@gmail.com>
2022-06-23 16:20:45 -0300

    cuentoelementos

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -128,7 +128,12 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+var cantidadmayora18=0;
+for (var i = 0; i < arreglo.length; i++) {
+  if(arreglo[i]>18)
+  cantidadmayora18++
+}
+return cantidadmayora18;
 }
 
 
