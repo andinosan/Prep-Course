fe432b9326572da97a27bd42c0c9311f56449cfa
andinosan <andinosan@gmail.com>
2022-06-24 16:23:31 -0300

    tieneemail

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -68,8 +68,8 @@ function tieneEmail (usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
-  if (usuario.email == true) return true
-  else return false
+  if (usuario.email){ return true;}
+  else {return false;}
 }
 
 function tienePropiedad (objeto, propiedad) {
